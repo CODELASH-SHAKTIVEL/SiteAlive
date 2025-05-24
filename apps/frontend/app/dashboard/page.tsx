a4c1cef0daf393d44caa36ef6d032f2fd8a08f89
JAVASCRIPT-LEARNING <shaktivel.elangovan2004@gmail.com>
2025-05-25 01:25:27 +0530

    Add dashboard component with welcome message

---
+++ apps/frontend/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <div>
+      dashboard
+      <p className="text-2xl">Welcome to the dashboard!</p>
+    </div>
+  )
+}
+
+export default dashboard
